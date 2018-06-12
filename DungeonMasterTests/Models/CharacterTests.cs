1514ee328553d4d2be170702e8c322faa157a863
Jason Felton <jasonfelton@MacBook.local>
2018-06-12 15:39:53 -0400

    Initial commit

---
+++ DungeonMasterTests/Models/CharacterTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace DungeonMasterTests.Models
+{
+    [TestFixture()]
+    public class CharacterTests
+    {
+        [Test()]
+        public void CanMakeNewCharacter()
+        {
+            Character character = new Character();
+        }
+    }
+}
