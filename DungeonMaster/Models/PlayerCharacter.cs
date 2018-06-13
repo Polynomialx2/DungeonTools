bfe24c9e8522f6d198edcb332df12593a9e0e56f
Jason Felton <jasonfelton@MacBook.local>
2018-06-12 20:14:56 -0400

    Added PlayerCharacter class

--- DungeonMaster/Models/PlayerCharacter.cs
+++ DungeonMaster/Models/PlayerCharacter.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace DungeonMaster.Models
 {
-    public class Character
+    public class PlayerCharacter : Character
     {
-        public Character()
+        private string _playerName;
+
+        public string PlayerName { get => _playerName; set => _playerName = value; }
+
+        public PlayerCharacter(string name, int initiative, string playerName)
         {
+            base.Name = name;
+            base.Initiative = initiative;
+            PlayerName = playerName;
         }
     }
 }
