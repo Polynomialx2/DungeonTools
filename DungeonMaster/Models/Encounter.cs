c2236e6ec60df7c3fd80ab56374876c06d159d4d
Jason Felton <jasonfelton@MacBook.local>
2018-06-13 17:27:32 -0400

    Add basic encounter start

--- DungeonMaster/Models/Encounter.cs
+++ DungeonMaster/Models/Encounter.cs
@@ -6,12 +6,16 @@ namespace DungeonMaster.Models
     public class Encounter
     {
         List<PlayerCharacter> _party = new List<PlayerCharacter>();
+        List<Monster> _monsters = new List<Monster>();
+        List<string> _initiativeOrder = new List<string>();
 
         public Encounter()
         {
         }
 
         public List<PlayerCharacter> Party { get => _party; }
+        public List<Monster> Monsters { get => _monsters; set => _monsters = value; }
+        public List<string> InitiativeOrder { get => _initiativeOrder; }
 
         /*
          * Adds player character to the party
@@ -23,6 +27,23 @@ namespace DungeonMaster.Models
 
         public void clearParty() {
             Party.Clear();
+        }
+
+        public void addMonster(Monster monster) {
+            Monsters.Add(monster);
+        }
+
+        public void clearMonsters() {
+            Monsters.Clear();
+        }
+
+        public void start() {
+            foreach (PlayerCharacter character in Party) {
+                _initiativeOrder.Add(character.Name);
+            }
+            foreach (Monster monster in _monsters) {
+                _initiativeOrder.Add(monster.Name);
+            }
         }
     }
 }
--- DungeonMasterTests/Models/EncounterTests.cs
+++ DungeonMasterTests/Models/EncounterTests.cs
@@ -25,5 +25,39 @@ namespace DungeonMasterTests.Models
             encounter.clearParty();
             Assert.IsTrue(encounter.Party.Count == 0);
         }
+
+        [Test()]
+        public void CanAddMonstersToEncounter() {
+            Encounter encounter = new Encounter();
+            Monster beholder = new Monster("Beholder", 6);
+            encounter.addMonster(beholder);
+            Assert.IsTrue(encounter.Monsters.Count == 1);
+        }
+
+        [Test()]
+        public void CanClearMonsters()
+        {
+            Encounter encounter = new Encounter();
+            Monster beholder = new Monster("Beholder", 6);
+            encounter.addMonster(beholder);
+            Assert.IsTrue(encounter.Monsters.Count == 1);
+            encounter.clearMonsters();
+            Assert.IsTrue(encounter.Monsters.Count == 0);
+        }
+
+        [Test()]
+        public void CanStartEncounter() {
+            Encounter encounter = setUpEncounter();
+            encounter.start();
+            Assert.IsTrue(encounter.InitiativeOrder.Count != 0);
+        }
+
+        private Encounter setUpEncounter() {
+            Encounter encounter = new Encounter();
+            encounter.addPC(new PlayerCharacter("Kresh", 10, "Jason"));
+            encounter.addPC(new PlayerCharacter("Gerald", 11, "Brian"));
+            encounter.addMonster(new Monster("Beholder", 6));
+            return encounter;
+        }
     }
 }
