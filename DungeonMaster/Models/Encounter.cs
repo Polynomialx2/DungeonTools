c706b716c68893995a4e77192ad28a6dce1b8e94
Jason Felton <jasonfelton@MacBook.local>
2018-06-13 16:46:53 -0400

    Add Party to encounter

---
+++ DungeonMaster/Models/Encounter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DungeonMaster.Models
+{
+    public class Encounter
+    {
+        List<PlayerCharacter> _party = new List<PlayerCharacter>();
+
+        public Encounter()
+        {
+        }
+
+        public List<PlayerCharacter> Party { get => _party; }
+
+        /*
+         * Adds player character to the party
+         */
+
+        public void addPC(PlayerCharacter character) {
+            Party.Add(character);
+        }
+
+        public void clearParty() {
+            Party.Clear();
+        }
+    }
+}
---
+++ DungeonMasterTests/Models/EncounterTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using DungeonMaster.Models;
+
+namespace DungeonMasterTests.Models
+{
+    [TestFixture()]
+    public class EncounterTests
+    {
+        [Test()]
+        public void CanAddPlayerCharactersToEncounter() {
+            Encounter encounter = new Encounter();
+            PlayerCharacter kresh = new PlayerCharacter("Kresh", 9, "Jason");
+            encounter.addPC(kresh);
+            Assert.IsTrue(encounter.Party.Count == 1);
+        }
+
+        [Test()]
+        public void CanClearParty()
+        {
+            Encounter encounter = new Encounter();
+            PlayerCharacter kresh = new PlayerCharacter("Kresh", 9, "Jason");
+            encounter.addPC(kresh);
+            Assert.IsTrue(encounter.Party.Count == 1);
+            encounter.clearParty();
+            Assert.IsTrue(encounter.Party.Count == 0);
+        }
+    }
+}
