e935976fd49b4626c4f2c64541f00d00b7a78856
Jason Felton <jasonfelton@MacBook.local>
2018-06-12 20:25:39 -0400

    Add Monster class

---
+++ DungeonMaster/Models/Monster.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace DungeonMaster.Models
+{
+    public class Monster : Character
+    {
+        public Monster(string name, int initiativeModifier)
+        {
+            base.Name = name;
+            base.InitiativeModifier = initiativeModifier;
+        }
+
+        public void rollInitiative() {
+            base.Initiative = InitiativeModifier;
+        }
+    }
+}
---
+++ DungeonMasterTests/Models/MonsterTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using DungeonMaster.Models;
+
+namespace DungeonMasterTests.Models
+{
+    [TestFixture()]
+    public class MonsterTests
+    {
+        [Test()]
+        public void CanRollInitiativeForMonster()
+        {
+            var beholder = new Monster("Beholder", 6);
+            beholder.rollInitiative();
+            Assert.AreNotEqual(beholder.Initiative, null);
+        }
+    }
+}
